e3e57820d06067f246aecb0ec5737e5b6a0cc0f8
willshepp28 <willsheppard28@outlook.com>
2017-12-05 17:47:31 -0500

    more queries

--- test.sql
+++ test.sql
@@ -7,6 +7,17 @@ createdb friend
 \l
 
 
+-- disconnect
+\q
+
+
+-- show a list of all tables in database
+\dt
+
+-- connect to database
+\c <db_name>
+
+
 
 -- create a table in datebase
 create table friends(id serial primary key, firstname char(50), lastname char(50), age int, city char(50));
@@ -26,6 +37,11 @@ select * from friends;
 
 -- filter rows based on a specific condition
 select * from friends where age > 23;
+select age from friends where age > 18;
+select name from friends where name = 'john';
+select city from friends where city != 'FL';
+select * from friends where age >= 22;
+select * from friends where age <= 22;
 
 
 -- counts how many firstnames in table we have
